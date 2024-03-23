f1bd3589a093e5855fce3929906dc46af5f70216
ifilmahd <65521650+ifilmahd@users.noreply.github.com>
2024-03-24 00:05:39 +0100

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,8 +1,8 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "John Doe" );
-define( "RECIPIENT_EMAIL", "example@gmail.com" ); //write your mail here
+define( "RECIPIENT_NAME", "DevStudioAl" );
+define( "RECIPIENT_EMAIL", "ifilmahd@gmail.com" ); //write your mail here
 
 // Read the form values
 $success = false;
